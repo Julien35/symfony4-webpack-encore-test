782a2d1e50f9342f485bae7b524a23235541196c
BERTHAU Julien <julien.berthau@orange.com>
2018-02-27 14:23:30 +0100

    add webpack HMR

--- webpack.config.js
+++ webpack.config.js
@@ -11,13 +11,14 @@ Encore
 
     // uncomment for legacy applications that require $/jQuery as a global variable
     .autoProvidejQuery()
-    // .autoProvideVariables({
-    // "window.jQuery": "jquery"
-    // "jQuery.tagsinput": "bootstrap-tagsinput"
-    // })
 
     // uncomment if you use Sass/SCSS files
-    .enableSassLoader()
+    // .enableSassLoader()
+    // parameters are not mandatory, only if webpack build is slow with bootstrap (http://symfony.com/doc/current/frontend/encore/bootstrap.html)
+    .enableSassLoader(function (sassOptions) {
+    }, {
+        resolveUrlLoader: false,
+    })
 
     .enableSourceMaps(!Encore.isProduction())
 
@@ -25,13 +26,13 @@ Encore
     .enableVersioning(Encore.isProduction())
 
     // uncomment to define the assets of the project
-    .addEntry('js/app', './assets/js/app.js')
-    .addEntry('js/web-cola-test', './assets/js/web-cola-test.js')
-    .addStyleEntry('css/app', './assets/scss/app.scss')
-
-    // .addStyleEntry('vendors', [
-    //
-    // ])
+    // uncomment to define the assets of the project
+    .addEntry('index',
+        [
+            './assets/js/app.js',
+            './assets/js/web-cola-test.js',
+            './assets/scss/app.scss'
+        ])
 
     // this creates a 'vendor.js' file with jquery and the bootstrap JS module
     // these modules will *not* be included in page1.js or page2.js anymore
@@ -48,7 +49,6 @@ Encore
         'boosted/dist/css/bootstrap-orangeIE82015.css',
         'boosted/dist/css/orangeHelvetica.css',
         'boosted/dist/css/orangeIcons.css',
-
         'bootstrap-datepicker/dist/css/bootstrap-datepicker.css'
     ])
 ;
